71b2e32c602cf2bac1cdd019209e88178c9304e9
Tomás Link <tomaslink@gmail.com>
2024-03-03 12:29:58 -0300

    arrays: zero_matrix

--- src/algorithmic/arrays.py
+++ src/algorithmic/arrays.py
@@ -24,3 +24,29 @@ def rotate_matrix(matrix: List[List]) -> None:
             matrix[end - offset][start] = matrix[end][end - offset]
             matrix[end][end - offset] = matrix[i][end]
             matrix[i][end] = top
+
+
+def zero_matrix(matrix: List[List]) -> None:
+    """Modifies a matrix (in-place), so that if an element of it is zero,
+        its entire row and column are set to zero.
+
+    Complexity:
+    - Time: O(N^2)
+    - Space: O(N^2)
+    """
+    rows = []
+    columns = []
+
+    for i, row in enumerate(matrix):
+        for j, cell in enumerate(row):
+            if cell == 0:
+                rows.append(i)
+                columns.append(j)
+
+    M = len(matrix[0])
+    for i in rows:
+        matrix[i] = [0 for _ in range(M)]
+
+    for column in columns:
+        for row in matrix:
+            row[column] = 0
--- tests/test_arrays.py
+++ tests/test_arrays.py
@@ -3,6 +3,12 @@ import pytest
 from algorithmic import arrays
 
 
+def print_matrix(matrix):
+    print("")
+    for s in matrix:
+        print(s)
+
+
 def test_rotate_matrix():
     matrix = [[1, 2], [3, 4]]
     arrays.rotate_matrix(matrix)
@@ -15,3 +21,21 @@ def test_rotate_matrix():
     matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     arrays.rotate_matrix(matrix)
     assert matrix == [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
+
+
+def test_zero_matrix():
+    matrix = [[1, 2], [3, 4]]
+    arrays.zero_matrix(matrix)
+    assert matrix == [[1, 2], [3, 4]]
+
+    matrix = [[1, 0], [3, 4]]
+    arrays.zero_matrix(matrix)
+    assert matrix == [[0, 0], [3, 0]]
+
+    matrix = [[1, 2, 3], [4, 0, 6], [7, 8, 9]]
+    arrays.zero_matrix(matrix)
+    assert matrix == [[1, 0, 3], [0, 0, 0], [7, 0, 9]]
+
+    matrix = [[1, 0, 0], [4, 5, 6], [7, 8, 9]]
+    arrays.zero_matrix(matrix)
+    assert matrix == [[0, 0, 0], [4, 0, 0], [7, 0, 0]]
